caf53b2d09c8cfb94fffee5c643bb291583c2d81
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-03-17 12:28:50 -0300

    detect button after update

--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,28 @@
 var elements
 
 window.onload = function() {
-    elements = loadPlayList()
-    addButtons()
+    document.querySelector("div#contents.style-scope.ytd-playlist-video-list-renderer.style-scope.ytd-playlist-video-list-renderer").addEventListener("DOMNodeInserted", function (a) {
+        //a.target.style.backgroundColor = "red"
+        console.log(a)
+        console.log(a.target.matches("#menu .yt-spec-icon-shape div"))
+        if (a.target.matches("#menu .yt-spec-icon-shape div")) {
+            a.target.style.backgroundColor = "red"
+            addButton(a.target)
+        }
+            //addButton(a.relatedNode.childNodes[0].querySelector("#menu #button.dropdown-trigger")))
+            //addButton(a.relatedNode.childNodes[0])
+        //elements = loadPlayList()
+        //addButtons()
+    })
 }
 
 chrome.runtime.onMessage.addListener(function (callback) {
-    elements = loadPlayList()
-    addButtons()
 })
 
-function loadPlayList() {
-    return document.querySelectorAll("#menu #button.dropdown-trigger")
-}
-
-function addButtons() {
-    if (elements) {
-        for (let element of elements) {
-            let btn = document.createElement("div")
-            btn.innerText = "Deletar"
-            btn.classList.add("youtube_manager_button")
-            element.parentNode.parentNode.append(btn)
-            console.log(btn)
-            /*element.style.backgroundColor = "yellow"*/
-        }
-    }
+function addButton(element) {
+    let btn = document.createElement("div")
+    btn.innerText = "Deletar"
+    btn.classList.add("youtube_manager_button")
+    element.parentNode.parentNode.append(btn)
+     /*element.style.backgroundColor = "yellow"*/
 }
